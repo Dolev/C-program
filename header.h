c5f0b0b920f5978def01decfa70def021f73377c
dolevh123 <33119992+dolevh123@users.noreply.github.com>
2017-12-29 14:43:10 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+//Define :
+#define MAX_ROW 6
+#define MAX_COLUMN 7
+#define Space ' '
+#define STACK_MAX 42 // 7*6 
+
+int Push(int data);
+int Pop();
+int Peek();
+int Isfull() ;
+int Isempty() ;
+void Print_Board(char board[6][7]);
+int Row(char board[MAX_ROW][MAX_COLUMN],char User [] ,int player);
+int Column(char board[MAX_ROW][MAX_COLUMN],char User [] ,int player);
+int Trace1(char board[MAX_ROW][MAX_COLUMN],char User [] ,int player);
+int Trace2(char board[MAX_ROW][MAX_COLUMN],char User [] ,int player);
+int Full_Column(char board[MAX_ROW][MAX_COLUMN],int n);
+void add(char board[MAX_ROW][MAX_COLUMN],int col,char[] ,int place);
